b4209f6b5a1a5587621866c811d88620837303ba
huncholane <huncho@hygo.ai>
2025-09-12 07:40:32 -0700

    FT command does current file with no args now

--- lua/ftconfig/commands.lua
+++ lua/ftconfig/commands.lua
@@ -1,7 +1,11 @@
 vim.api.nvim_create_user_command("FT", function(args)
-	require("ftconfig").edit_filetype(args.fargs[1])
+	if args.fargs[1] then
+		require("ftconfig").edit_filetype(args.fargs[1])
+	else
+		require("ftconfig").edit_filetype(vim.bo.filetype)
+	end
 end, {
-	nargs = 1,
+	nargs = "*",
 	desc = "Edit or create config for given filetype",
 	complete = function(lead, _, _)
 		local options = vim.fn.getcompletion("", "filetype")
